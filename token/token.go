74cf0dbea776a657895dc424825235a97b854806
Matthew Butler <butler.matthew@gmail.com>
2018-07-28 11:47:26 -0300

    Start with tokens.

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+// TokenType is the constant type of a token.
+type TokenType string
+
+// Token is the individual token including type and the string literal which composes that type.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	Illegal = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers & literals
+	Ident = "IDENT"
+	Int   = "INT"
+
+	// Operators
+	Assign = "="
+	Plus   = "+"
+
+	// Delimiters
+	Comma     = ","
+	Semicolon = ";"
+
+	LParen = "("
+	RParen = ")"
+	LBrace = "{"
+	RBrace = "}"
+
+	// Keywords
+	Function = "FUNCTION"
+	Let      = "LET"
+)
