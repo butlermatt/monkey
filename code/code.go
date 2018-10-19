d741b82bebbb8944f01ab228cddd63fb5ec57245
Matthew Butler <butler.matthew@gmail.com>
2018-10-19 10:51:05 -0300

    Add debugging information

--- code/code.go
+++ code/code.go
@@ -1,16 +1,53 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
-type Instructions []byte
 type OpCode byte
 
 const (
 	OpConstant OpCode = iota
 )
+
+type Instructions []byte
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			_, _ = fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, ins[i+1:])
+		_, _ = fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+
+		i += 1 + read
+	}
+
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	count := len(def.OperandWidths)
+
+	if len(operands) != count {
+		return fmt.Sprintf("ERROR: operand length %d does not match defined %d\n", len(operands), count)
+	}
+
+	switch count {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount (%d) for %s\n", count, def.Name)
+}
 
 type Definition struct {
 	Name          string
@@ -56,4 +93,24 @@ func Make(op OpCode, operands ...int) []byte {
 	}
 
 	return instruction
+}
+
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	oper := make([]int, len(def.OperandWidths))
+	offset := 0
+
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			oper[i] = int(ReadUint16(ins[offset:]))
+		}
+
+		offset += width
+	}
+
+	return oper, offset
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
--- code/code_test.go
+++ code/code_test.go
@@ -28,3 +28,58 @@ func TestMake(t *testing.T) {
 		})
 	}
 }
+
+func TestInstructionsString(t *testing.T) {
+	inst := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatted := Instructions{}
+	for _, ins := range inst {
+		concatted = append(concatted, ins...)
+	}
+
+	if concatted.String() != expected {
+		t.Errorf("instructions formatted incorrectly. expected=%q, got=%q", expected, concatted.String())
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		name     string
+		op       OpCode
+		operands []int
+		read     int
+	}{
+		{"OpConstant", OpConstant, []int{65535}, 2},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			inst := Make(tt.op, tt.operands...)
+
+			def, err := Lookup(byte(tt.op))
+			if err != nil {
+				t.Fatalf("Opcode definition not found: %q\n", err)
+			}
+
+			opsRead, n := ReadOperands(def, inst[1:])
+			if n != tt.read {
+				t.Fatalf("wrong number of bytes read. expected=%d, got=%d", tt.read, n)
+			}
+
+			for i, want := range tt.operands {
+				if opsRead[i] != want {
+					t.Errorf("wrong operand. expected=%d, got=%d", want, opsRead[i])
+				}
+			}
+		})
+	}
+}
